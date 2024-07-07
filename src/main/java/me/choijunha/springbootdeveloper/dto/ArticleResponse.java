32cc339e4ce39cbe86134f7dedca123ff77281ae
JUNHA CHOI <choijh0309@naver.com>
2024-07-07 14:12:33 +0900

    글 목록 조회 update

---
+++ src/main/java/me/choijunha/springbootdeveloper/dto/ArticleResponse.java
@@ -0,0 +1,16 @@
+package me.choijunha.springbootdeveloper.dto;
+
+import lombok.Getter;
+import me.choijunha.springbootdeveloper.domain.Article;
+
+@Getter
+public class ArticleResponse {
+
+    private final String title;
+    private final String content;
+
+    public ArticleResponse(Article article) {
+        this.title = article.getTitle();
+        this.content = article.getContent();
+    }
+}
