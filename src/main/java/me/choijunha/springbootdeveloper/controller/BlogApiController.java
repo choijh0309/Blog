3cd5d77b6de5c1b0f96b7f2799579ad957007e09
JUNHA CHOI <choijh0309@naver.com>
2024-07-07 15:38:34 +0900

    글 수정 기능 update

--- src/main/java/me/choijunha/springbootdeveloper/controller/BlogApiController.java
+++ src/main/java/me/choijunha/springbootdeveloper/controller/BlogApiController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import me.choijunha.springbootdeveloper.domain.Article;
 import me.choijunha.springbootdeveloper.dto.AddArticleRequest;
 import me.choijunha.springbootdeveloper.dto.ArticleResponse;
+import me.choijunha.springbootdeveloper.dto.UpdateArticleRequest;
 import me.choijunha.springbootdeveloper.service.BlogService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -53,5 +54,14 @@ public class BlogApiController {
 
         return ResponseEntity.ok()
                 .build();
+    }
+
+    @PutMapping("/api/articles/{id}")
+    public ResponseEntity<Article> updateArticle(@PathVariable long id,
+                                                 @RequestBody UpdateArticleRequest request) {
+        Article updateArticle = blogService.update(id, request);
+
+        return ResponseEntity.ok()
+                .body(updateArticle);
     }
 }
--- src/main/java/me/choijunha/springbootdeveloper/service/BlogService.java
+++ src/main/java/me/choijunha/springbootdeveloper/service/BlogService.java
@@ -1,8 +1,10 @@
 package me.choijunha.springbootdeveloper.service;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import me.choijunha.springbootdeveloper.domain.Article;
 import me.choijunha.springbootdeveloper.dto.AddArticleRequest;
+import me.choijunha.springbootdeveloper.dto.UpdateArticleRequest;
 import me.choijunha.springbootdeveloper.repository.BlogRepository;
 import org.springframework.stereotype.Service;
 
@@ -29,5 +31,15 @@ public class BlogService {
 
     public void delete(long id) {
         blogRepository.deleteById(id);
+    }
+
+    @Transactional
+    public Article update(long id, UpdateArticleRequest request) {
+        Article article = blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found: " + id));
+
+        article.update(request.getTitle(), request.getContent());
+
+        return article;
     }
 }
