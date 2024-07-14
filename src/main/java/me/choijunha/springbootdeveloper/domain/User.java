57e6b93131b4ea66e31fa2c8d401765267f51c2a
JUNHA CHOI <choijh0309@naver.com>
2024-07-14 16:41:46 +0900

    oAuth2 구현 적용 OAuth2 서비스 구현 update

--- src/main/java/me/choijunha/springbootdeveloper/domain/User.java
+++ src/main/java/me/choijunha/springbootdeveloper/domain/User.java
@@ -29,10 +29,20 @@ public class User implements UserDetails { // UserDetails 를 상속받아 인�
     @Column(name = "password")
     private String password;
 
+    @Column(name = "nickname", unique = true)
+    private String nickname;
+
     @Builder
-    public User(String email, String password, String auth) {
+    public User(String email, String password, String nickname) {
         this.email = email;
         this.password = password;
+        this.nickname = nickname;
+    }
+
+    public User update(String nickname) {
+        this.nickname = nickname;
+
+        return this;
     }
 
     @Override
@@ -69,6 +79,4 @@ public class User implements UserDetails { // UserDetails 를 상속받아 인�
     public boolean isEnabled() {
         return true;
     }
-
-
 }
